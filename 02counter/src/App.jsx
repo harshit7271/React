b530b75b7dc31e6989a1c12dd5b0f75a1659b089
Harshit Singh <harshitsingh05893312@gmail.com>
2025-12-20 20:55:18 +0530

    Updated just now

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,19 @@
+
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Garima aur Mai</h1>
+      <h2>Counter Value : 5</h2>
+      <button>Add Value</button>
+      <button>Remove Value</button>
+    </>
+  )
+}
+
+export default App
