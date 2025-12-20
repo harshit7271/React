fe3fb98a61af42af35402c371cd33ce13dd70b1e
Harshit Singh <harshitsingh05893312@gmail.com>
2025-12-20 19:18:11 +0530

    Updated main.jsx

--- 01vitereactbasics/src/main.jsx
+++ 01vitereactbasics/src/main.jsx
@@ -1,9 +1,37 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React, { Children } from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
- 
-    <App />
-    
+export default function MyApp() {
+    return (
+        <>
+            <h1>Garima is the best</h1>
+        </>
+    )
+}
+
+/*
+const ReactElement = {
+    type : 'a',
+    props : {
+        href : 'https://google.com',
+        target : '_blank',
+    },
+        children : 'Click here to visit Google'
+}
+*/
+
+/*
+const anotherElement = (
+    <a href="https://www.google.com" target='_blank'>Click here to visit Google</a>
+)
+*/
+
+const reactElement = React.createElement(
+    'a',
+    {href: 'https://google.com', target: '_blank'},
+    'Click here to visit Google'
+)
+ReactDOM.createRoot(document.getElementById('root')).render(
+    reactElement
 )
