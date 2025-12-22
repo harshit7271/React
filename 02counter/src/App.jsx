cc0aa9d53187bcc26171af94eaee5ac2a81fa3e1
Harshit Singh <harshitsingh05893312@gmail.com>
2025-12-23 04:27:06 +0530

    Updated recentaly

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,17 +1,26 @@
 // ALL ABOUT HOOKS IN REACT
 
+import { useState } from 'react'      // 1. useState is a hook that updates evry time the state changes.
 
-// import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-
-  let counter = 5
   
-  const addValue = () => {
-    console.log("Value Added", Math.random());
+  let [counter, gudiiCounter]= useState(15)          // 2. useState returns an array with two elements: the current state value and a function to update it.
+
+  // let counter = 15
+  
+const addValue = () => {
+  gudiiCounter(counter + 1);
+  console.log("Value Added", counter)
+  // counter = counter + 1 
+}
+
+const removeValue = () => {
+  gudiiCounter(counter - 1)
+  console.log("Value Removed", counter)
 }
   return (
     <>
@@ -19,9 +28,11 @@ function App() {
       <h2>Counter Value : {counter}</h2>
       <button
       onClick ={addValue}
-      >Add Value</button>
+      >Add Value {counter}</button>
       <br />
-      <button>Remove Value</button>
+      <button onClick = {removeValue}
+      >Remove Value: {counter}</button>
+      <p>footer: {counter}</p>
     </>
   )
 }
