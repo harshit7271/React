e49c2dee9d0d736e4ed6e9853767227da08386c8
Harshit Singh <harshitsingh05893312@gmail.com>
2025-12-24 21:12:18 +0530

    Update

--- 03TailwindProps/tailwind.config.js
+++ 03TailwindProps/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",                                  
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],                                            // Specify the paths to all of the template files in our project
   theme: {
     extend: {},
   },
