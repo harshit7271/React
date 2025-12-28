5d36a465bf927622ba5aca248e982ffe13eb8224
Harshit Singh <harshitsingh05893312@gmail.com>
2025-12-28 19:07:56 +0530

    Update

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -7,16 +7,16 @@ function App() {
     <div 
       className="w-full h-screen duration-200"
       style={{ backgroundColor: color }}
-    >
-      <div className = "fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+    > 
+      <div className = "fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">     
         <div className = "flex flex-wrap justify-center gap-3 bg-lg bg-white px-3 py-2 rounded-2xl">
-
+       
           <button onClick={() => setColor("red")}         // onClick expects us to pass a function & () callback is also a function which is calling setColor with "red"
           className="outline-none bg-red-500 text-white px-4 py-1 
           rounded-full shadow-lg"
           style={{ backgroundColor: "red" }}>Red</button>
 
-          <button onClick={() => setColor("green")}
+          <button onClick={() => setColor("green")}      
           className="outline-none bg-green-500 text-white px-4 py-1 
           rounded-full shadow-lg"
           style={{ backgroundColor: "green" }}>Green</button>
@@ -37,3 +37,9 @@ function App() {
   )
 }
 export default App
+
+
+// note :   fixed positioning to keep the buttons at the bottom of the page, inset-x-0 to center it horizontally, px-2 for horizontal padding
+// flex flex-wrap justify-center to arrange buttons in a row and center them, gap-3 for spacing between buttons
+// bg-lg bg-white px-3 py-2 rounded-2xl for styling the button container with background color, padding, and rounded corners
+// Padding : It adds space inside the button around the text, making the button larger and easier to click.
