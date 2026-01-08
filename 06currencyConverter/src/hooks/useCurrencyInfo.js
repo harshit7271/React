d2aff660d075086514924ed24c933185fc1d758b
Harshit Singh <harshitsingh05893312@gmail.com>
2026-01-08 22:52:12 +0530

    API call

---
+++ 06currencyConverter/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,8 @@
+import { useEffect, useState } from "react"
+
+
+function useCurrencyInfo(currency){
+    useEffect(() => {
+        fetch()
+    }, [])
+}           // with useEffect fetch will be called automatically
