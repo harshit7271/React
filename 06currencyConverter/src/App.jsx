76b7033398e0dc4af2b525cd20032a7f818e0694
Harshit Singh <harshitsingh05893312@gmail.com>
2026-01-09 22:52:35 +0530

    Updated Recently

--- 06currencyConverter/src/App.jsx
+++ 06currencyConverter/src/App.jsx
@@ -8,6 +8,21 @@ function App() {
   const [amount, setAmount] = useState(0)
   const [from, setFrom] = useState("usd")
   const [to, setTo] = useState("inr")
+  const [convertedAmount, setConvertedAmount] = useState(0)
+
+  const currencyInfo = useCurrencyInfo(from)
+
+  const options = Object.keys(currencyInfo)
+
+  const swap = () => {
+    setFrom(to)
+    setTo(from)
+    setConvertedAmount(amount)
+    setAmount(convertedAmount)
+  }
+  const convert = () => {
+    setConvertedAmount(amount * currencyInfo[to])
+  }
 
   return (
     <>
